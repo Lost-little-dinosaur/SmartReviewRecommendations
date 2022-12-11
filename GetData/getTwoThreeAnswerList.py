c6c474f9a13c2875eca71a123f076d98dc387d04
Lost-little-dinosaur <3144794112@qq.com>
2022-12-11 13:34:44 +0800

    add getTwoThreeAnswerList

---
+++ GetData/getTwoThreeAnswerList.py
@@ -0,0 +1,48 @@
+import os
+from math import exp
+import json
+import pickle
+
+
+def getTwoThreeAnswerList():
+    twoAnswerQuestionSetDict = {}
+    threeAnswerQuestionSetDict = {}
+    dirPath = "AllUsersAnswers/"
+    num = 0
+    for each in os.listdir(dirPath):
+        with open(dirPath + each, 'rb') as f:
+            twoAnswerArr = []
+            threeAnswerArr = []
+            questionSetUserAnswerArr = pickle.load(f)
+            # print(questionSetUserAnswerArr)
+            for eachUserAnswer in questionSetUserAnswerArr:
+                for eachQuestion in eachUserAnswer.keys():
+                    if len(eachUserAnswer[eachQuestion]) == 2:
+                        tempArr = eachUserAnswer[eachQuestion]
+                        # 按照时间戳升序排序
+                        tempArr.sort(key=lambda x: x[1])
+                        twoAnswerArr.append([tempArr[0][0], tempArr[1][0], tempArr[1][1] - tempArr[0][1]])
+                    elif len(eachUserAnswer[eachQuestion]) == 3:
+                        tempArr = eachUserAnswer[eachQuestion]
+                        # 按照时间戳排序
+                        tempArr.sort(key=lambda x: x[1])
+                        threeAnswerArr.append(
+                            [tempArr[0][0], tempArr[1][0], tempArr[2][0], tempArr[1][1] - tempArr[0][1],
+                             tempArr[2][1] - tempArr[1][1]])
+                    elif len(eachUserAnswer[eachQuestion]) > 3:
+                        num += 1
+            twoAnswerQuestionSetDict[each] = twoAnswerArr
+            threeAnswerQuestionSetDict[each] = threeAnswerArr
+    return twoAnswerQuestionSetDict, threeAnswerQuestionSetDict
+
+
+if __name__ == '__main__':
+    twoAnswerQuestionSetDict, threeAnswerQuestionSetDict = getTwoThreeAnswerList()
+    # 将变量userAnswerArr序列化到本地
+    dirPath = "TwoThreeAnswerQuestionSetDict/"
+    filePath = "TwoAnswerList.pkl"
+    with open(dirPath + filePath, 'wb') as f:
+        pickle.dump(twoAnswerQuestionSetDict, f)
+    filePath = "ThreeAnswerList.pkl"
+    with open(dirPath + filePath, 'wb') as f:
+        pickle.dump(threeAnswerQuestionSetDict, f)
