c65125ceddf819bd3b886384d42ce8c19999d717
Lost-little-dinosaur <3144794112@qq.com>
2022-12-11 15:43:07 +0800

    fix 3 into 2

--- GetData/getTwoThreeAnswerList.py
+++ GetData/getTwoThreeAnswerList.py
@@ -8,9 +8,9 @@ def getTwoThreeAnswerList():
     twoAnswerQuestionSetDict = {}
     threeAnswerQuestionSetDict = {}
     dirPath = "AllUsersAnswers/"
-    num = 0
     for each in os.listdir(dirPath):
         with open(dirPath + each, 'rb') as f:
+
             twoAnswerArr = []
             threeAnswerArr = []
             questionSetUserAnswerArr = pickle.load(f)
@@ -22,15 +22,16 @@ def getTwoThreeAnswerList():
                         # 按照时间戳升序排序
                         tempArr.sort(key=lambda x: x[1])
                         twoAnswerArr.append([tempArr[0][0], tempArr[1][0], tempArr[1][1] - tempArr[0][1]])
-                    elif len(eachUserAnswer[eachQuestion]) == 3:
+                    elif len(eachUserAnswer[eachQuestion]) >= 3:
                         tempArr = eachUserAnswer[eachQuestion]
                         # 按照时间戳排序
                         tempArr.sort(key=lambda x: x[1])
+                        tempArr2 = tempArr[0:2]
+                        twoAnswerArr.append([tempArr2[0][0], tempArr2[1][0], tempArr2[1][1] - tempArr2[0][1]])
+                        tempArr3 = tempArr[0:3]
                         threeAnswerArr.append(
-                            [tempArr[0][0], tempArr[1][0], tempArr[2][0], tempArr[1][1] - tempArr[0][1],
-                             tempArr[2][1] - tempArr[1][1]])
-                    elif len(eachUserAnswer[eachQuestion]) > 3:
-                        num += 1
+                            [tempArr3[0][0], tempArr3[1][0], tempArr3[2][0], tempArr3[1][1] - tempArr3[0][1],
+                             tempArr3[2][1] - tempArr3[1][1]])
             twoAnswerQuestionSetDict[each] = twoAnswerArr
             threeAnswerQuestionSetDict[each] = threeAnswerArr
     return twoAnswerQuestionSetDict, threeAnswerQuestionSetDict
