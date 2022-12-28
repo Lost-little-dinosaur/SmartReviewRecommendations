09badc1f0999d2db38aee8b96ab970f8d604c189
unknown <771509792@qq.com>
2022-12-28 17:28:51 +0800

    update curves and parameters save in excel

--- Simulation/myPlot.py
+++ Simulation/myPlot.py
@@ -1,13 +1,15 @@
 from math import e
 
 from matplotlib import pyplot as plt
+import SmartReviewRecommendations.EbbinghausForgettingCurve.EbbinghausCurves as Eb
+
 
 
 def main():
     a11 = -2.2515171935807575e-06
     b11 = -0.03234073841959593
-    a10 = -1.2467053258896204e-05
-    b10 = -0.13903372905214134
+    a10 = -1.2467053257506555e-05
+    b10 = -0.13903372905754122
     a01 = -8.57839280720141e-06
     b01 = -0.0890746714685621
     a00 = -1.2833467689157539e-05
@@ -16,20 +18,55 @@ def main():
     b1 = -0.06476805277713989
     a0 = -5.033426596650367e-06
     b0 = -0.2784718873971984
-    x = [i for i in range(0, 100000000, 100000)]
-    y11 = [e ** (a11 * i + b11) for i in x]
-    y10 = [e ** (a10 * i + b10) for i in x]
-    y01 = [e ** (a01 * i + b01) for i in x]
-    y00 = [e ** (a00 * i + b00) for i in x]
-    y1 = [e ** (a1 * i + b1) for i in x]
-    y0 = [e ** (a0 * i + b0) for i in x]
-    plt.plot(x, y11, label='11')
-    plt.plot(x, y10, label='10')
-    plt.plot(x, y01, label='01')
-    plt.plot(x, y00, label='00')
-    plt.plot(x, y1, label='1')
-    plt.plot(x, y0, label='0')
-    plt.legend()
+    t = range(10,1000000,1)
+    t1 = range(10,10000000,1)
+    t2 = range(10,100000,1)
+    # 字体
+    plt.rcParams['font.sans-serif'] = ['SimHei']
+    plt.grid(True, linestyle="--", color="g", linewidth="0.5")
+    plt.tick_params(labelsize=11)
+    y11 = [e ** (a11 * i + b11) for i in t]
+    y10 = [e ** (a10 * i + b10) for i in t2]
+    y01 = [e ** (a01 * i + b01) for i in t2]
+    y00 = [e ** (a00 * i + b00) for i in t2]
+    y1 = [e ** (a1 * i + b1) for i in t1]
+    y0 = [e ** (a0 * i + b0) for i in t]
+
+    plt.scatter(Eb.result_T[:,0], Eb.result_T[:,1], s=30, marker='o')
+    plt.title("第一次做对", fontsize=13)
+    plt.xlabel('t（min）', fontsize=12)
+    plt.ylabel('memory_rate（%）', fontsize=12)
+    plt.plot(t1, y1, label='1')
+    plt.show()
+
+    plt.grid(True, linestyle="--", color="g", linewidth="0.5")
+    plt.scatter(Eb.result_F[:, 0], Eb.result_F[:, 1], s=30, marker='o')
+    plt.title("第一次做错", fontsize=13)
+    plt.plot(t, y0, label='0')
+    plt.show()
+
+    plt.grid(True, linestyle="--", color="g", linewidth="0.5")
+    plt.scatter(Eb.result_T_T[:, 0], Eb.result_T_T[:, 1], s=30, marker='o')
+    plt.title("第一次做对，第二次做对", fontsize=13)
+    plt.plot(t, y11, label='11')
+    plt.show()
+
+    plt.grid(True, linestyle="--", color="g", linewidth="0.5")
+    plt.scatter(Eb.result_T_F[:, 0], Eb.result_T_F[:, 1], s=30, marker='o')
+    plt.title("第一次做对，第二次做错", fontsize=13)
+    plt.plot(t2, y10, label='10')
+    plt.show()
+
+    plt.grid(True, linestyle="--", color="g", linewidth="0.5")
+    plt.scatter(Eb.result_F_T[:, 0], Eb.result_F_T[:, 1], s=30, marker='o')
+    plt.title("第一次做错，第二次做对", fontsize=13)
+    plt.plot(t2, y01, label='01')
+    plt.show()
+
+    plt.grid(True, linestyle="--", color="g", linewidth="0.5")
+    plt.scatter(Eb.result_F_F[:, 0], Eb.result_F_F[:, 1], s=30, marker='o')
+    plt.title("第一次做错，第二次做错", fontsize=13)
+    plt.plot(t2, y00, label='10')
     plt.show()
 
 
