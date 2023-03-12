310a539a36f57894c1b64f52d0eeac25a02fba82
unknown <771509792@qq.com>
2023-03-12 12:35:08 +0800

    update

--- EbbinghausForgettingCurve/NewCurves.py
+++ EbbinghausForgettingCurve/NewCurves.py
@@ -13,11 +13,13 @@ dirPath_SAQSD = '../GetData/SeveralAnswerQuestionSetDict/'
 dirPath_All_Correct_rate = '../GetData/AllQuestionsCorrectRate/'
 dirpath_Allusers_Answers = '../GetData/AllUsersAnswers/'
 
+##将所有用户的答题记录读入到AllAnswers中，此为字典结构
 with open(dirPath_SAQSD + 'SeveralAnswerQuestionSetDict.pkl', 'rb') as f1:
     AllAnswerData = pickle.load(f1)
 
 
 
+##将所有的题目正确率读入AllCorrectRate中
 files = os.listdir(dirPath_All_Correct_rate)
 AllCorrectRate = {}
 for file in files:
@@ -25,9 +27,16 @@ for file in files:
         CorrectRate = pickle.load(f1)
         AllCorrectRate.update(CorrectRate)
 
-files = os.listdir(dirpath_Allusers_Answers)
-AllAnswers = {}
-for file in files:
-    with open(dirpath_Allusers_Answers + file, 'rb') as f2:
-        Answers = pickle.load(f2)
-        AllAnswers.update(Answers)
+
+# files = os.listdir(dirpath_Allusers_Answers)
+# AllAnswers = {}
+# for file in files:
+#     with open(dirpath_Allusers_Answers + file, 'rb') as f2:
+#         Answers = pickle.load(f2)
+#         AllAnswers.update(Answers)
+
+print(type(AllAnswerData[2]))
+MaxTimeStamp = []
+
+for key in AllAnswerData.keys():
+    MaxTimeStamp.append(max(AllAnswerData[key][:(-key+1)]))
